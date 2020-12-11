f8fb3f42bd801403f8daaca0b9dc174398139613
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2020-12-11 14:08:36 +0900

    delete_routes.rb_resources_groups_index

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :create, :new, :edit, :update]
 end
