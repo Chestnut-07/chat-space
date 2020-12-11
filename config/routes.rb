aeae4734d4c10d00cc0d503e38a6ba2faf8097b3
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2020-12-11 14:09:47 +0900

    add_routes.rb_groups_nest_resources_chats_index

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "groups#index"
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:create, :new, :edit, :update] do
+    resources :chats, only: [:index, :create]
 end
