636ad552fb41a3df793bde5d26b37169ead8dcb0
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2020-12-13 16:19:00 +0900

    add_application.rb_config.action_view.field_error_proc

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module ChatSpace
       config.i18n.default_locale = :ja
       config.time_zone = 'Tokyo'
     end
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
