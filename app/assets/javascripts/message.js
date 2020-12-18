f06a69d2b299e433e0dd553177cc55010134f51a
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2020-12-18 15:31:07 +0900

    add_message.js_buildHTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,46 @@
 $(function(){
+  function buildHTML(message){
+    //[もしメッセージに画像が含まれていたら]という条件式
+    if (message.image) {
+      let html = //メッセージに画像が含まれる場合のHTMLを作る
+        `<div class="message-box">
+          <div class="message-info">
+            <div class="message-info__name">
+              ${message.user_name}
+            </div>
+            <div class="message-info__date">
+                ${message.created_at}
+            </div>
+          </div>
+          <div class="message-main">
+            <p class="message-main__text">
+              ${message.content}
+            </p>
+            <img class="message-main_image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =　//メッセージに画像が含まれない場合のHTMLを作る
+      `<div class="message-box">
+      <div class="message-info">
+        <div class="message-info__name">
+          ${message.user_name}
+        </div>
+        <div class="message-info__date">
+            ${message.created_at}
+        </div>
+      </div>
+      <div class="message-main">
+        <p class="message-main__text">
+          ${message.content}
+        </p>
+      </div>
+    </div>`
+    return html;
+    };
+  }
+
   $('.form').on("submit",function(e){
     e.preventDefault();
     let formData = new FormData(this);
