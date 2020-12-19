11a28e62aad1b73dc2a9ae6f8d997ad31838db7e
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2020-12-19 14:10:06 +0900

    add_ javascripts/users.js_ajax

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,9 @@
 $(function() {
   $("#UserSearch__field").on("keyup", function() {
     let input = $("#UserSearch__field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
