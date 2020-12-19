70b779fd2d697ad22854f640f81f868cc90d8a59
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2020-12-19 14:11:14 +0900

    javascripts/users.js_done_if

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,6 +9,16 @@ $(function() {
     })
     .done(function(users) {
       $("#UserSearchResult").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          addUser(user);
+        });
+      } else if (input.length == 0) {
+          return false;
+      } else {
+          addNoUser();
+      }
+    })
     .fail(function() {
       alert("通信エラーです。ユーザーが表示できません。");
     });
