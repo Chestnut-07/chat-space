72fa4a9aaddecab4c34752826f0b1644a507dc38
Chestnut-07 <75065421+Chestnut-07@users.noreply.github.com>
2020-12-20 15:12:11 +0900

    add_javascripts/message.js_if-messaged.lrngth_not0

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -75,6 +75,17 @@ $(function(){
 
     .done(function(messages) {
       // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        let insertHTML = '';
+        //配列messagesの中身を一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(message, function(i, message) {
+          insertHTML += buildHTML(message) 
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.message-list').append(insertHTML);
+        $('.message-list').animate({ scrollTOP: $('.message-list')[0].scrollHeight});
+      }
     })
     .fail(function() {
       alert('error');
